60ee97cebea7d839111a8fbc9655bb4a4d4956d9
ashish1113 <ashishkiit42@gmail.com>
2019-07-27 21:21:36 +0530

    middleware-commit

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -16,29 +16,26 @@ let isAuthorized = (req, res, next) => {
     if (req.params.authToken || req.query.authToken || req.body.authToken || req.header('authToken')) {
         Auth.findOne({ authToken: req.header('authToken') || req.params.authToken || req.body.authToken || req.query.authToken }, (err, authDetails) => {
             if (err) {
-                console.log(err)
                 logger.error(err.message, 'AuthorizationMiddleware', 10)
                 let apiResponse = responseLib.generate(true, 'Failed To Authorized', 500, null)
                 res.send(apiResponse)
             } else if (check.isEmpty(authDetails)) {
-
                 logger.error('No AuthorizationKey Is Present', 'AuthorizationMiddleware', 10)
                 let apiResponse = responseLib.generate(true, 'Invalid Or Expired AuthorizationKey', 404, null)
                 res.send(apiResponse)
             } else {
                 token.verifyToken(authDetails.authToken, authDetails.tokenSecret, (err, decoded) => {
-
                     if (err) {
                         logger.error(err.message, 'Authorization Middleware', 10)
                         let apiResponse = responseLib.generate(true, 'Failed To Authorized', 500, null)
                         res.send(apiResponse)
                     }
                     else {
-                        req.user = { userId: decoded.data.userId}
+                        req.user = { userId: decoded.data.userId }
                         next();
                         console.log(req.user);
 
-                        
+
                     }
 
 
--- app/middlewares/routeLogger.js
+++ app/middlewares/routeLogger.js
@@ -10,7 +10,6 @@ let requestIpLogger = (req, res, next) => {
     console.log('!OPTIONS');
     var headers = {};
     // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
     headers["Access-Control-Allow-Origin"] = "*";
     headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
     headers["Access-Control-Allow-Credentials"] = false;
@@ -20,7 +19,6 @@ let requestIpLogger = (req, res, next) => {
     res.end();
   }
   else {
-
     // enable or disable cors here 
     res.header("Access-Control-Allow-Origin", appConfig.allowedCorsOrigin);
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
@@ -28,7 +26,6 @@ let requestIpLogger = (req, res, next) => {
 
     next();
   }
-
 
 }// end request ip logger function  
 
